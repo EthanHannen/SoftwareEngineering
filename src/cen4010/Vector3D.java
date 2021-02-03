9312e421a4756e634ef4e0b639326dfcb4646b37
Cryptexx <ethanhannen@outlook.com>
2021-02-03 01:09:28 -0500

    Rebuilding to base Project 1 files & directories

---
+++ src/cen4010/Vector3D.java
@@ -0,0 +1,9 @@
+package cen4010;
+
+public class Vector3D {
+
+    public static void main(String[] args)
+    {
+        System.out.println("This is the main method");
+    }
+}
---
+++ test/cen4010/Vector3DTest.java
@@ -0,0 +1,14 @@
+package cen4010;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Vector3DTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
